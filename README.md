826d41f0aa91e82a21e313c3315bda5e7498180c
ztsalexey <zthalexey@gmail.com>
2023-03-07 17:39:30 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NFTerium early rewards NFT
+
+Drop is deployed at 0x9B686Ecb0a07FfFA47Bcb6d3AC9320F00D054F3D  
---
+++ helper-hardhat-config.js
@@ -0,0 +1,23 @@
+const { ethers } = require("hardhat")
+
+const networkConfig = {
+    31337: {
+        name: "hardhat",
+    },
+    5: {
+        name: "goerli",
+    },
+    11155111: {
+        name: "sepolia",
+    },
+    1: {
+        name: "mainnet",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
